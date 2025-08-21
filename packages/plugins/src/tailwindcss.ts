c1adcf6d318f505de5e1db25be6982a43870f69e
gz1382 <liangpingbo@4399.com>
2025-08-21 19:40:24 +0800

    feat(export): 升级tailwindcss 为4

--- packages/plugins/src/tailwindcss.ts
+++ packages/plugins/src/tailwindcss.ts
@@ -73,6 +73,6 @@ function getTailwindBinPath(opts: { cwd: string }) {
   const pkgPath = require.resolve('tailwindcss/package.json', {
     paths: [opts.cwd],
   });
-  const tailwindPath = require(pkgPath).exports['.']['import'];
+  const tailwindPath = require(pkgPath).exports['./plugin']['require'];
   return join(dirname(pkgPath), tailwindPath);
 }
